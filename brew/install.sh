1a6fc8d343396e10f3ecaf75a746c8c8c2cc5685
Suchan An <dobestan@Suchans-MacBook-Pro.local>
2015-03-20 03:18:32 +0900

    init brew : adding install script to install brew and brew cask

---
+++ brew/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+# Install Homebrew
+# http://brew.sh
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+# Install Homebrew Cask
+# http://caskroom.io
+
+brew install caskroom/cask/brew-cask
