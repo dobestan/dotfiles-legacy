e7871c1816bc7103883d48892a3f4d61cfa5af46
dobestan <dobestan@gmail.com>
2015-03-20 15:35:55 +0900

    install Vundle : vim plugin manager, and NerdTree

--- symlink.sh
+++ symlink.sh
@@ -2,3 +2,6 @@ ln -s /Users/dobestan/.dotfiles/.common_profile ~/
 ln -s /Users/dobestan/.dotfiles/.aliases ~/
 ln -s /Users/dobestan/.dotfiles/.bash_profile ~/
 ln -s /Users/dobestan/.dotfiles/.zshrc ~/
+
+ln -s /Users/dobestan/.dotfiles/.vim ~/
+ln -s /Users/dobestan/.dotfiles/.vim/.vimrc ~/
