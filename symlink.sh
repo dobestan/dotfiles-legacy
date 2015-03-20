d9c4b0837c4349726a5adb34c40e390cb3af79d8
dobestan <dobestan@gmail.com>
2015-03-20 15:05:18 +0900

    add custom symlinking files to manage more easily

---
+++ symlink.sh
@@ -0,0 +1,4 @@
+ln -s /Users/dobestan/.dotfiles/.common_profile ~/
+ln -s /Users/dobestan/.dotfiles/.aliases ~/
+ln -s /Users/dobestan/.dotfiles/.bash_profile ~/
+ln -s /Users/dobestan/.dotfiles/.zshrc ~/
